9cf11d6894ffae2b346f78748c9834d16cd9153f
Ryan Dahl <ry@tinyclouds.org>
2022-02-23 07:29:01 -0500

    Fix cjs_exports_multi

--- src/lib.rs
+++ src/lib.rs
@@ -1,128 +1,14 @@
+mod package_json;
+mod parse_specifier;
+
+use package_json::PackageJson;
+use parse_specifier::parse_specifier;
 use serde_json::Map;
 use serde_json::Value;
 use std::path::Path;
 use std::path::PathBuf;
 
-#[derive(Clone, Debug)]
-struct PackageConfig {
-  exists: bool,
-  exports: Option<Value>,
-  imports: Option<Map<String, Value>>,
-  main: Option<String>,
-  name: Option<String>,
-  pjsonpath: PathBuf,
-  typ: String,
-}
-
-fn get_package_config(
-  path: PathBuf,
-  /*
-    specifier: &str,
-    maybe_base: Option<&ModuleSpecifier>,
-  */
-) -> anyhow::Result<PackageConfig> {
-  // TODO(bartlomieju):
-  // if let Some(existing) = package_json_cache.get(path) {
-  //   return Ok(existing.clone());
-  // }
-
-  let result = std::fs::read_to_string(&path);
-
-  let source = result.unwrap_or_else(|_| "".to_string());
-  if source.is_empty() {
-    let package_config = PackageConfig {
-      pjsonpath: path,
-      exists: false,
-      main: None,
-      name: None,
-      typ: "none".to_string(),
-      exports: None,
-      imports: None,
-    };
-    // TODO(bartlomieju):
-    // package_json_cache.set(package_json_path, package_config.clone());
-    return Ok(package_config);
-  }
-
-  let package_json: Value = serde_json::from_str(&source).map_err(|err| {
-    /*
-          let base_msg = maybe_base.map(|base| {
-            format!("\"{}\" from {}", specifier, to_file_path(base).display())
-          });
-    errors::err_invalid_package_config(
-      &path.display().to_string(),
-      base_msg,
-      Some(err.to_string()),
-    )
-    */
-    anyhow::anyhow!("malformed json")
-  })?;
-
-  let imports_val = package_json.get("imports");
-  let main_val = package_json.get("main");
-  let name_val = package_json.get("name");
-  let typ_val = package_json.get("type");
-  let exports = package_json.get("exports").map(|e| e.to_owned());
-
-  let imports = if let Some(imp) = imports_val {
-    imp.as_object().map(|imp| imp.to_owned())
-  } else {
-    None
-  };
-  let main = if let Some(m) = main_val {
-    m.as_str().map(|m| m.to_string())
-  } else {
-    None
-  };
-  let name = if let Some(n) = name_val {
-    n.as_str().map(|n| n.to_string())
-  } else {
-    None
-  };
-
-  // Ignore unknown types for forwards compatibility
-  let typ = if let Some(t) = typ_val {
-    if let Some(t) = t.as_str() {
-      if t != "module" && t != "commonjs" {
-        "none".to_string()
-      } else {
-        t.to_string()
-      }
-    } else {
-      "none".to_string()
-    }
-  } else {
-    "none".to_string()
-  };
-
-  let package_config = PackageConfig {
-    pjsonpath: path,
-    exists: true,
-    main,
-    name,
-    typ,
-    exports,
-    imports,
-  };
-  // TODO(bartlomieju):
-  // package_json_cache.set(package_json_path, package_config.clone());
-  Ok(package_config)
-}
-
-fn file_extension_probe(mut p: PathBuf) -> anyhow::Result<PathBuf> {
-  if p.exists() {
-    return Ok(p);
-  } else {
-    p.set_extension("js");
-    if p.exists() {
-      return Ok(p);
-    } else {
-      return Err(not_found());
-    }
-  }
-}
-
-fn node_resolve(
+pub fn node_resolve(
   specifier: &str,
   referrer: &Path,
   conditions: &[&str],
@@ -141,55 +27,34 @@ fn node_resolve(
 
   // We've got a bare specifier or maybe bare_specifier/blah.js"
 
-  let (bare, maybe_rest) = if let Some((bare, rest)) = specifier.split_once("/")
-  {
-    (bare, Some(rest))
-  } else {
-    (specifier, None)
-  };
+  let (package_name, package_subpath, is_scoped) =
+    parse_specifier(specifier).unwrap();
 
   for ancestor in referrer.ancestors() {
     // println!("ancestor {:?}", ancestor);
-    let module_dir = ancestor.join("node_modules").join(bare);
+    let module_dir = ancestor.join("node_modules").join(&package_name);
     let package_json_path = module_dir.join("package.json");
     if package_json_path.exists() {
-      //println!("path_json_path {:?}", package_json_path);
-      let package_config = get_package_config(package_json_path)?;
+      let package_json = PackageJson::load(package_json_path)?;
 
-      println!("package_config {:#?}", package_config.exports);
+      if let Some(map) = package_json.exports_map {
+        // println!("package_subpath {}", package_subpath);
+        // println!("package_json exports_map {:#?}", map);
 
-      match package_config.exports {
-        Some(Value::String(s)) => {
-          return Ok(module_dir.join(s));
-        }
-        Some(Value::Object(map)) => {
-          for condition in conditions {
-            if let Some(x) = map.get(&condition.to_string()) {
-              if let Value::String(s) = x {
-                return Ok(module_dir.join(s));
-              }
-            }
-          }
+        if let Some((key, subpath)) = exports_resolve(&map, &package_subpath) {
+          let value = map.get(&key).unwrap();
+          let s = conditions_resolve(value, conditions);
 
-          if let Some(x) = map.get(".") {
-            if let Value::String(s) = x {
-              return Ok(module_dir.join(s));
-            }
-          }
-
+          let t = resolve_package_target_string(&s, subpath);
+          return Ok(module_dir.join(t));
+        } else {
           todo!()
-        }
-        Some(_) => {
-          todo!();
-        }
-        None => {
-          // pass thru
         }
       }
 
       // old school
-      if let Some(rest) = maybe_rest {
-        let d = module_dir.join(rest);
+      if package_subpath != "." {
+        let d = module_dir.join(package_subpath);
         if let Ok(m) = d.metadata() {
           if m.is_dir() {
             return Ok(d.join("index.js"));
@@ -197,7 +62,7 @@ fn node_resolve(
         }
         return file_extension_probe(d);
       } else {
-        if let Some(main) = package_config.main {
+        if let Some(main) = package_json.main {
           return Ok(module_dir.join(main));
         } else {
           return Ok(module_dir.join("index.js"));
@@ -207,6 +72,98 @@ fn node_resolve(
   }
 
   Err(not_found())
+}
+
+// TODO needs some unit tests.
+fn resolve_package_target_string(
+  target: &str,
+  subpath: Option<String>,
+) -> String {
+  if let Some(subpath) = subpath {
+    // println!("target {}", target);
+    // println!("subpath {:?}", subpath);
+    target.replace('*', &subpath)
+  } else {
+    target.to_string()
+  }
+}
+
+fn conditions_resolve(value: &Value, conditions: &[&str]) -> String {
+  match value {
+    Value::String(s) => s.to_string(),
+    Value::Object(map) => {
+      for condition in conditions {
+        if let Some(x) = map.get(&condition.to_string()) {
+          if let Value::String(s) = x {
+            return s.to_string();
+          } else {
+            todo!()
+          }
+        }
+      }
+      todo!()
+    }
+    _ => todo!(),
+  }
+}
+
+fn exports_resolve(
+  map: &Map<String, Value>,
+  subpath: &str,
+) -> Option<(String, Option<String>)> {
+  if map.contains_key(subpath) {
+    return Some((subpath.to_string(), None));
+  }
+
+  // best match
+  let mut best_match = None;
+  for key in map.keys() {
+    if let Some(pattern_index) = key.find('*') {
+      let key_sub = &key[0..pattern_index];
+      //println!("subpath {}", subpath);
+      //println!("key_sub {}", key_sub);
+
+      if subpath.starts_with(key_sub) {
+        if subpath.ends_with('/') {
+          todo!()
+        }
+        let pattern_trailer = &key[pattern_index + 1..];
+        //println!("pattern_trailer {}", pattern_trailer);
+
+        if subpath.len() > key.len()
+          && subpath.ends_with(pattern_trailer)
+          // && pattern_key_compare(best_match, key) == 1
+          && key.rfind('*') == Some(pattern_index)
+        {
+          let rest = subpath
+            [pattern_index..(subpath.len() - pattern_trailer.len())]
+            .to_string();
+          best_match = Some((key, rest));
+        }
+      }
+    }
+  }
+
+  if let Some((key, subpath_)) = best_match {
+    // println!("key {}", key);
+    //println!("subpath_ {}", subpath_);
+    return Some((key.to_string(), Some(subpath_)));
+  }
+
+  None
+}
+
+fn file_extension_probe(mut p: PathBuf) -> anyhow::Result<PathBuf> {
+  if p.exists() {
+    return Ok(p);
+  } else {
+    p.set_extension("js");
+    if p.exists() {
+      return Ok(p);
+    } else {
+      return Err(not_found());
+    }
+  }
 }
 
 fn not_found() -> anyhow::Error {
---
+++ src/parse_specifier.rs
@@ -0,0 +1,73 @@
+pub fn parse_specifier(specifier: &str) -> Option<(String, String, bool)> {
+  let mut separator_index = specifier.find('/');
+  let mut valid_package_name = true;
+  let mut is_scoped = false;
+  if specifier.is_empty() {
+    valid_package_name = false;
+  } else if specifier.starts_with('@') {
+    is_scoped = true;
+    if let Some(index) = separator_index {
+      separator_index = specifier[index + 1..].find('/');
+    } else {
+      valid_package_name = false;
+    }
+  }
+
+  let package_name = if let Some(index) = separator_index {
+    specifier[0..index].to_string()
+  } else {
+    specifier.to_string()
+  };
+
+  // Package name cannot have leading . and cannot have percent-encoding or separators.
+  for ch in package_name.chars() {
+    if ch == '%' || ch == '\\' {
+      valid_package_name = false;
+      break;
+    }
+  }
+
+  if !valid_package_name {
+    return None;
+  }
+
+  let package_subpath = if let Some(index) = separator_index {
+    format!(".{}", specifier.chars().skip(index).collect::<String>())
+  } else {
+    ".".to_string()
+  };
+
+  Some((package_name, package_subpath, is_scoped))
+}
+
+#[test]
+fn test_parse_specifier() {
+  let cases = vec![
+    (
+      "@parcel/css-darwin-arm64",
+      Some(("@parcel/css-darwin-arm64", ".", true)),
+    ),
+    (
+      "./path/to/directory",
+      Some((".", "./path/to/directory", false)),
+    ),
+    ("a-package/foo", Some(("a-package", "./foo", false))),
+    ("a-package/m.mjs", Some(("a-package", "./m.mjs", false))),
+    (
+      "es-module-package/features/x",
+      Some(("es-module-package", "./features/x", false)),
+    ),
+  ];
+  for (input, expected) in cases {
+    let r = parse_specifier(input);
+    if let Some((package_name, subpath, is_scoped)) = expected {
+      let actual = r.unwrap();
+      assert_eq!(actual.0, package_name);
+      assert!(actual.1.starts_with("./") || actual.1 == ".");
+      assert_eq!(actual.1, subpath);
+      assert_eq!(actual.2, is_scoped);
+    } else {
+      assert!(r.is_none());
+    }
+  }
+}
